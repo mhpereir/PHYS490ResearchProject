f85ebd2b48de58555baaf09f3d808c0ee2c529f6
mhpereir <mhpereir@edu.uwaterloo.ca>
2020-03-14 14:48:10 -0400

    improved I/O and working selection tree in data_utils. Runs on laptop!

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 import torch
 
+import torch.nn as nn
+
 from time import time
 from cnn import StarNet
 from data_utils import Data
@@ -11,31 +13,35 @@ if __name__ == '__main__':
     start_time = time()
     # Command line arguments (taken from tutorial script (lesson 5))
     parser = argparse.ArgumentParser(description='Final Project: CNN script')
-    parser.add_argument('--train_path', metavar='inputs',
-                        help='training data file name (hdf5)')
-    parser.add_argument('--test_path', metavar='inputs',
-                        help='test data file name (hdf5)')
-    parser.add_argument('--params_path', metavar='params',
-                        help='hyper params file name (json)')
-    parser.add_argument('--output_path', metavar='results', default='results/',
+    parser.add_argument('--data_path', metavar='path',
+                        help='path to data directory')
+    parser.add_argument('--train', metavar='str',
+                        help='which training dataset to use: real or synth')
+    parser.add_argument('--test', metavar='str',
+                        help='which testing dataset to use: real or synth')
+    parser.add_argument('-o', metavar='results', default='results/',
                         help='path to results')
     parser.add_argument('-v', type=int, default=2, metavar='N',
                         help='verbosity (default: 2)')
-    parser.add_argument('-cuda', type=int, default=0, metavar='N',
+    parser.add_argument('-c', type=int, default=0, metavar='N',
                        help='cuda indicator (default: 0 = OFF)')
     args = parser.parse_args()
     
-    train_file_path  = args.train_path
-    test_file_path   = args.test_path
-    params_file_path = args.params_path
-    output_file_path = args.output_path
-    cuda_input       = args.cuda
+    data_file_path   = args.data_path
+    train_data       = args.train
+    test_data        = args.test
+    output_path      = args.o
+    verb             = args.v
+    cuda_input       = args.c
+    
+    params_file_path = str(data_file_path) + '/params.json'
+    
     
     with open(params_file_path) as paramfile:
         param_file = json.load(paramfile)
     
-    data  = Data(train_file_path, test_file_path)
-    model = StarNet().double()
+    data  = Data(data_file_path, train_data, test_data, param_file['n_cross'])
+    model = StarNet().float()
     
     model.init_data(data,cuda_input)
     
@@ -47,24 +53,24 @@ if __name__ == '__main__':
     cross_vals = []
     num_epochs = int(param_file['n_epoch'])
     
+    #model = nn.DataParallel(model)
     # Training loop
     for epoch in range(1, num_epochs + 1):
         train_val = model.backprop(loss, optimizer, n_train=param_file['n_mini_batch'])
         obj_vals.append(train_val)
         
-        
-        test_val = model.test(loss)
-        cross_vals.append(test_val)
+        cross_val = model.cross(loss)
+        cross_vals.append(cross_val)
         
         # High verbosity report in output stream
         if args.v>=2:
             if not ((epoch + 1) % int(param_file['n_epoch_v'])):
                 print('Epoch [{}/{}]'.format(epoch+1, num_epochs)+\
                       '\tTraining Loss: {:.4f}'.format(train_val)+\
-                      '\tTest Loss: {:.4f}'.format(test_val))
+                      '\tTest Loss: {:.4f}'.format(cross_val))
     
     # Low verbosity final report
-    if args.v:
+    if args.v>=1:
         print('Final training loss: {:.4f}'.format(obj_vals[-1]))
         print('Final test loss: {:.4f}'.format(cross_vals[-1]))
     
@@ -75,7 +81,7 @@ if __name__ == '__main__':
     ax.plot(range(num_epochs), obj_vals, label= "Training loss", color="blue")
     ax.plot(range(num_epochs), cross_vals, label= "Test loss", color= "green")
     ax.legend()
-    fig.savefig(args.output_path + 'fig.pdf')
+    fig.savefig('results/loss.pdf')
     plt.close()
     
     #model.predict_test()
