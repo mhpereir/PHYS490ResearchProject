156b736316a37046c68405a179eb8077da84c219
Shreyas <44416610+ShreyasShankar@users.noreply.github.com>
2020-03-29 21:40:34 -0400

    refactored

--- main.py
+++ main.py
@@ -37,15 +37,14 @@ def get_args():
 
 
 def run_main():
-    # start_time = time()
-
+    # Load and extract CLI arguments
     args = get_args()
 
     data_path        = args.data_path
     train_data       = args.train
     test_data        = args.test
     output_path      = args.output_path
-    verb             = args.verbose
+    verbose          = args.verbose
     cuda_input       = args.cuda
 
     # Load hyperparameters from file
@@ -56,94 +55,94 @@ def run_main():
     num_epochs_v = int(params['n_epoch_v'])
     n_train      = int(params['n_mini_batch'])
 
+    # CUDA usage
+    device = torch.device('cuda' if torch.cuda.is_available() and args.cuda else 'cpu')
+    print('\nRunning on {}\n'.format(device))
+
     # Load in the training datasets
-    # REVIEW data utils
     data  = Data(data_path, train_data, test_data)
     data.load_train(params['n_cross'])
 
     # Initialize the model
     # REVIEW cnn
     model = StarNet().float()
+    model.to(device)
 
     # Define an optimizer and the loss function
     optimizer  = optim.Adam(model.parameters(), lr=params['lr'])
+    loss       = torch.nn.MSELoss()
 
+    # Define LR scheduler and its parameters
     reduce_lr_factor         = 0.5
     reduce_lr_patience       = 2
     reduce_lr_min            = 0.00008
-    reduce_lr_threshold      = 0.0009        #assuming this is the same as reduce_lr_epsilon in original code....????
+    reduce_lr_threshold      = 0.0009
     reduce_lr_threshold_mode = 'abs'
+
+    scheduler  = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
+                                                      factor=reduce_lr_factor,
+                                                      patience=reduce_lr_patience,
+                                                      min_lr=reduce_lr_min,
+                                                      threshold=reduce_lr_threshold,
+                                                      threshold_mode=reduce_lr_threshold_mode,
+                                                      verbose=True)
 
     early_stop_patience = 4
     early_stop_min_diff = 0.0001
-
-    scheduler  = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
-                                                    factor=reduce_lr_factor,
-                                                    patience=reduce_lr_patience,
-                                                    min_lr=reduce_lr_min,
-                                                    #eps=reduce_lr_epsilon,
-                                                    threshold=reduce_lr_threshold,
-                                                    threshold_mode=reduce_lr_threshold_mode,
-                                                    verbose=True)
-    loss       = torch.nn.MSELoss()
+    early_stop_condition = False
 
     obj_vals     = []
     cross_vals   = []
-    early_stop_condition = False
-
-    if torch.cuda.is_available() and cuda_input == 1:
-        device = 'cuda'
-        print('Running with CUDA')
-    else:
-        device = 'cpu'
-        print('Running with CPU')
-
-    model.to(device)
 
     # Training loop
-    for epoch in range(1, num_epochs + 1):
-        train_val,time_epoch = model.backprop(data, loss, optimizer, n_train, device)
-        obj_vals.append(train_val)
+    print('Training over {} epochs:'.format(num_epochs))
+    with tqdm(total=epochs, dynamic_ncols=True) as pbar:
+        for epoch in range(1, num_epochs + 1):
+            # Training
+            train_val, time_epoch = model.backprop(data, loss, optimizer, n_train, device)
+            obj_vals.append(train_val)
 
+            # Cross-validation evaluation
+            cross_val = model.cross(data, loss, device)
+            cross_vals.append(cross_val)
 
-        cross_val = model.cross(data, loss, device)
-        cross_vals.append(cross_val)
+            scheduler.step(train_val)
 
-        scheduler.step(train_val)
+            # Update progress bar
+            pbar.update()
 
-        # High verbosity report in output stream
-        if args.v>=2:
-            if not ((epoch) % num_epochs_v):
-                print('Epoch [{}/{}]'.format(epoch, num_epochs)+\
-                      '\tTraining Loss: {:.4f}'.format(train_val)+\
-                      '\tTest Loss: {:.4f}'.format(cross_val)+\
-                       '\tEllapsed time: {:.4f}m'.format(time_epoch))
+            # High verbosity report in output stream
+            if verbose:
+                if not ((epoch) % num_epochs_v):
+                    print('Epoch [{}/{}]'.format(epoch, num_epochs)+\
+                          '\tTraining Loss: {:.4f}'.format(train_val)+\
+                          '\tTest Loss: {:.4f}'.format(cross_val)+\
+                          '\tEpoch duration: {:.4f}m'.format(time_epoch))
 
-        early_stop_condition = model.check_early_stop(cross_vals, patience=early_stop_patience, min_diff=early_stop_min_diff)
+            early_stop_condition = model.check_early_stop(cross_vals, patience=early_stop_patience, min_diff=early_stop_min_diff)
 
-        if early_stop_condition:
-
-            print('Early stop condition met. Breaking at epoch: {}'.format(epoch))
-
-            break
+            if early_stop_condition:
+                print('Early stopping condition met. Breaking at epoch: {}'.format(epoch))
+                break
 
     # Low verbosity final report
-    if args.v>=1:
-        print('Final training loss: {:.4f}'.format(obj_vals[-1]))
-        print('Final test loss: {:.4f}'.format(cross_vals[-1]))
+    print('Final training loss: {:.4f}'.format(obj_vals[-1]))
+    print('Final test loss: {:.4f}'.format(cross_vals[-1]))
 
-    # print('Training time ellapsed: {:.4f}m'.format( (time()-start_time)/60) )
-
-    # Plot saved in results folder
-    fig,ax = plt.subplots()
-    ax.plot(range(epoch), obj_vals, label= "Training loss", color="blue")
-    ax.plot(range(epoch), cross_vals, label= "Test loss", color= "green")
-    ax.legend()
-    fig.savefig('results/loss.pdf')
-    plt.close()
-
+    # Plot loss and save to file
+    plt.plot(range(1, epoch), obj_vals, label= 'Training loss', color='blue')
+    plt.plot(range(1, epoch), cross_vals, label= 'CV loss', color= 'green')
+    plt.title('Cross-Validation Loss over %i Training Epochs' % epochs)
+    plt.ylabel('Loss')
+    plt.xlabel('Epochs')
+    plt.legend()
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+    plt.savefig(os.path.join(output_path, 'loss.png'), dpi=400)
+    plt.show()
 
     data.close('train')
+
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
     ## beginning of post processing
 
@@ -152,7 +151,7 @@ def run_main():
     predicted_targets = data.re_normalize_targets(predicted_targets)
     real_targets = data.y_test
     snr = data.snr
-    real_targets = np.concatenate((real_targets,snr),axis=1)
+    real_targets = np.concatenate((real_targets, snr),axis=1)
     data.close('test')
 
     respath = 'results'
